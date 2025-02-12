29dc306cce5272925c67795bfb4f84ffe635f99d
Rutuja Kirad <chinmayikirad31@gmail.com>
2025-02-12 21:21:30 +0530

    4th Iteration added included UseEffect

--- src/Message.js
+++ src/Message.js
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+
 export default function Message({ count }) {
-    return <p>The count is {count % 2 === 0 ? "Even" : "Odd"}.</p>;
-  }
-  
+  const [message, setMessage] = useState("The count is neutral.");
+
+  useEffect(() => {
+    setMessage(`The count is ${count % 2 === 0 ? "Even" : "Odd"}.`);
+  }, [count]);
+
+  return <p>{message}</p>;
+}
